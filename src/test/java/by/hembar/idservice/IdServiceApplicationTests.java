cd6e448f2174cd67bca707dcd881027cb48fc1b0
hembar <hsciapan@gmail.com>
2025-03-25 13:14:15 +0300

    Init project

---
+++ src/test/java/by/hembar/idservice/IdServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.hembar.idservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
